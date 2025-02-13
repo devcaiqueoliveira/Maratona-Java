460d69e49adfed91fe6fcbae76e467aca30be3a5
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-02-13 17:42:14 -0300

    chama

--- Maratona-Java/src/academy/devedojo/maratonajava/aulaOperadores.java
+++ Maratona-Java/src/academy/devedojo/maratonajava/aulaOperadores.java
@@ -1,4 +1,22 @@
 package academy.devedojo.maratonajava;
 
 public class aulaOperadores {
+    public static void main(String[] args) {
+        int numero01 = 10;
+        int numero02 = 20;
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+
+        System.out.println("Dez é maior do que vinte? " + isDezMaiorQueVinte);
+        System.out.println("Dez é menor do que vinte? " + isDezMenorQueVinte);
+
+
+        System.out.println("Dez é igual vinte? " + isDezIgualVinte);
+        System.out.println("Dez é igual dez? " + isDezIgualDez);
+        System.out.println("Dez é diferente de dez? " + isDezDiferenteDez);
+    }
 }
