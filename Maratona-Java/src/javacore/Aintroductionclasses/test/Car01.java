4947db7b2eaed8bfd61155469cdddad64455abe7
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-02-17 17:22:37 -0300

    ai dento

--- Maratona-Java/src/javacore/Aintroductionclasses/test/Car01.java
+++ Maratona-Java/src/javacore/Aintroductionclasses/test/Car01.java
@@ -9,7 +9,17 @@ public class Car01 {
         car.model = "88";
         car.year = 1988;
 
+        Car car02 = new Car();
+        car02.name = "Ferrari";
+        car02.model = "La ferrari";
+        car02.year = 2025;
+        System.out.println("Car 1: ");
         System.out.println(car.name + " " + car.model + " " + car.year);
+
+        System.out.println();
+        System.out.println("Car 2: ");
+        System.out.println(car02.name + " " + car02.model + " " + car02.year);
+
     }
 
 }
