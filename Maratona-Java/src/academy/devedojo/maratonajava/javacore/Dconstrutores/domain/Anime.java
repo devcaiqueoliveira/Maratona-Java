198ed6d1f9d3f3005588529a3beb5e788b1ad1b8
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-02-21 21:19:11 -0300

    .

--- Maratona-Java/src/academy/devedojo/maratonajava/javacore/Dconstrutores/domain/Anime.java
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Dconstrutores/domain/Anime.java
@@ -1,4 +1,10 @@
 package academy.devedojo.maratonajava.javacore.Dconstrutores.domain;
 
+import academy.devedojo.maratonajava.javacore.Dconstrutores.test.AnimeTest01;
+
 public class Anime {
+    public static void main(String[] args) {
+        AnimeTest01 anime = new AnimeTest01("One Piece", "Ação", "TV", 12);
+        anime.printar();
+    }
 }
--- Maratona-Java/src/academy/devedojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
@@ -1,4 +1,17 @@
 package academy.devedojo.maratonajava.javacore.Dconstrutores.test;
 
 public class AnimeTest01 {
+
+    private String name, genero, tipo;
+    private int epsodios;
+
+    public AnimeTest01 (String name, String genero, String tipo, int epsodios) {
+        this.name = name;
+        this.genero = genero;
+        this.tipo = tipo;
+        this.epsodios = epsodios;
+    }
+    public void printar() {
+        System.out.println(toString());
+    }
 }
