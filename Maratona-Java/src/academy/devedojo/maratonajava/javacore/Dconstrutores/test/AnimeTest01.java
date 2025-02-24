3786d646cef99501e68fc78001e28775b351cbac
Caique <devcaiqueoliveira@gmail.com>
2025-02-24 08:45:24 -0300

    .

--- Maratona-Java/src/academy/devedojo/maratonajava/javacore/Dconstrutores/test/AnimeTest01.java
+++
@@ -1,8 +0,0 @@
-package academy.devedojo.maratonajava.javacore.Dconstrutores.test;
-import academy.devedojo.maratonajava.javacore.Dconstrutores.domain.Anime;
-
-public class AnimeTest01 {
-    public static void main(String[] args) {
-
-    }
-}
