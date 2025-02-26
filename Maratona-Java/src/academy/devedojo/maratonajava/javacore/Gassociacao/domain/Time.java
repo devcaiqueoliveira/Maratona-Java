897267f0c5c18d5d07736c27001cfb215bad32a2
Caique <devcaiqueoliveira@gmail.com>
2025-02-26 10:33:30 -0300

    associacao

--- Maratona-Java/src/academy/devedojo/maratonajava/javacore/Gassociacao/domain/Time.java
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Gassociacao/domain/Time.java
@@ -2,9 +2,32 @@ package academy.devedojo.maratonajava.javacore.Gassociacao.domain;
 
 public class Time {
     private String name;
+    private Jogador[] jogadores;
 
     public Time(String name) {
         this.name = name;
+    }
+
+    public Time(String name, Jogador[] jogadores) {
+        this.name = name;
+        this.jogadores = jogadores;
+    }
+
+    public void imprime() {
+        System.out.println(this.name);
+        if (jogadores == null) return;
+        for (Jogador jogador : jogadores) {
+            System.out.println(jogador.getName());
+        }
+
+    }
+
+    public Jogador[] getJogadores() {
+        return jogadores;
+    }
+
+    public void setJogadores(Jogador[] jogadores) {
+        this.jogadores = jogadores;
     }
 
     public String getName() {
