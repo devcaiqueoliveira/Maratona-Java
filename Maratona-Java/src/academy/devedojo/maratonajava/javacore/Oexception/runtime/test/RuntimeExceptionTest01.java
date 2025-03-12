172a5802fdafe5f16d2b7ee511be84d2e1433e3f
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-03-12 16:19:44 -0300

    RuntimeException

---
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -0,0 +1,9 @@
+package academy.devedojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        // CHECKED E UNCHECKED
+        Object object = null;
+        System.out.println(object.toString());
+    }
+}
