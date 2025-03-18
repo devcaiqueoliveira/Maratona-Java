61dd6b42f1c993d91fc48c0e61d1529dfb70a52a
Caique <devcaiqueoliveira@gmail.com>
2025-03-18 10:43:01 -0300

    Lançando excecoes pt1

---
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Oexception/runtime/test/RunTimeTest02.java
@@ -0,0 +1,22 @@
+package academy.devedojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RunTimeTest02 {
+    public static void main(String[] args) {
+        divisao(1,0);
+        System.out.println("teste");
+    }
+    public static int divisao(int a, int b) {
+        if (b == 0){
+            throw new IllegalArgumentException("Não pode ser 0");
+        }
+        return a/b;
+
+        /* try {
+            return a/b;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        return 0; */
+
+    }
+}
