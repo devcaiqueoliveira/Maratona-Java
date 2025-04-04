c019be62cf93e203d37228b7af7cf5b9af60a458
Caique <devcaiqueoliveira@gmail.com>
2025-04-04 09:40:28 -0300

    Simple Date Format e Local Date pt1

---
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Rdatas/test/LocalDateTest01.java
@@ -0,0 +1,11 @@
+package academy.devedojo.maratonajava.javacore.Rdatas.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+    }
+}
