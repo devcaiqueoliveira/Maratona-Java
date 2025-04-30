cedfedfb9b14b61c86c3d938fa425b5a195c5cdb
Caique <devcaiqueoliveira@gmail.com>
2025-04-30 09:03:16 -0300

    .

---
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
@@ -0,0 +1,10 @@
+package academy.devedojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalDateTime;
+
+public class DurationTest01 {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime nowAF = LocalDateTime.now();
+    }
+}
