ab22debf79075de62be84a7b2c264828aa0161c7
Caique Oliveira <devcaiqueoliveira@gmail.com>
2025-05-22 16:22:13 -0300

    Equals + HashCode + List

--- Maratona-Java/src/academy/devedojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ Maratona-Java/src/academy/devedojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -1,5 +1,7 @@
 package academy.devedojo.maratonajava.javacore.Ycolecoes.dominio;
 
+import java.util.Objects;
+
 public class Smartphone {
     private String serialNumber;
     private String marca;
@@ -21,6 +23,15 @@ public class Smartphone {
         if (this.getClass() != obj.getClass()) return false;
         Smartphone smartphone = (Smartphone) obj;
         return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    // se x.equals(y) == true, y.hashCode() == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false o hashCode precisa ser diferente
+    // y.hashCode() != x.hashCode() x.equals(y) deverá ser false.
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
     }
 
     public String getSerialNumber() {
